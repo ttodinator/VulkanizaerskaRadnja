c095ff5fbe33ff52b469726a6b5b98fe62d8dc84
ttodinator <ttodinator@gmail.com>
2021-03-21 21:23:51 +0100

    Dodata JUnit test klasa za klasu AutoGuma, nije implementirana!

---
+++ VulkanizerskaRadnja/test/auto_radnja/gume/AutoGumaTest.java
@@ -0,0 +1,68 @@
+package auto_radnja.gume;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AutoGumaTest {
+	
+	AutoGuma a;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		a=new AutoGuma();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		a=null;
+	}
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAutoGuma() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAutoGumaStringIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMarkaModel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPrecnik() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSirina() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetVisina() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
