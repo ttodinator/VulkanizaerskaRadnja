834efa426c8b13d0481b6164eac077c37b0e9f3f
ttodinator <ttodinator@gmail.com>
2021-03-21 23:17:03 +0100

    Dokumentovani intefejs Radnja i klasa VulkanizerskaRadnja

--- VulkanizerskaRadnja/src/auto_radnja/Radnja.java
+++ VulkanizerskaRadnja/src/auto_radnja/Radnja.java
@@ -4,9 +4,31 @@ import java.util.LinkedList;
 
 import auto_radnja.gume.AutoGuma;
 
+/**
+ * Interfejs koji predstavlja radnju
+ * 
+ * @author Petar Todic
+ * 
+ * @version 1.0
+ *
+ */
 public interface Radnja {
-	
+	/**
+	 * Dodaje novu automobilsku gumu u radnju
+	 * 
+	 * @param a Automobilska guma koja se dodaje
+	 * 
+	 * @throws java.lang.NullPointerException ako je uneta guma null
+	 * @throws java.lang.RuntimeException ako je guma vec postoji u radnji
+	 */
 	void dodajGumu(AutoGuma a);
+	
+	/**
+	 * Pronalazi i vraca listu objekata tipa AutoGuma koje imaju isti naziv (marku i model) kao i String parametar
+	 * 
+	 * @param markaModel String vrednosti preko koje se vrsi pretraga guma
+	 * @return LinkedList Vraca listu objekata tipa AutoGuma
+	 */
 	LinkedList<AutoGuma> pronadjiGumu(String markaModel);
 
 }
--- VulkanizerskaRadnja/src/auto_radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/src/auto_radnja/VulkanizerskaRadnja.java
@@ -3,9 +3,18 @@ package auto_radnja;
 import java.util.LinkedList;
 
 import auto_radnja.gume.AutoGuma;
-
+/**
+ * Ova klasa implementira interfejs Radnja i predstavlja implementaciju preko liste
+ * 
+ * @author Petar Todic
+ * 
+ * @version 1.0
+ *
+ */
 public class VulkanizerskaRadnja implements Radnja{
-	
+	/**
+	 * List sa svim gumama u radnji
+	 */
 	private LinkedList<AutoGuma> gume=new LinkedList<AutoGuma>();
 
 	@Override
@@ -24,7 +33,7 @@ public class VulkanizerskaRadnja implements Radnja{
 			return null;
 		LinkedList<AutoGuma> novaLista = new LinkedList<AutoGuma>();
 		for(int i=0;i<gume.size();i++)
-			if (gume.get(i).getMarkaModel().equals(markaModel))
+			if (gume.get(i).getMarkaModel().contains(markaModel))
 				novaLista.add(gume.get(i));
 		return novaLista;
 	}
